90427bc8aa82857a11865580740ede342d503ab4
Carol Finke <carol.finke@kellogg.northwestern.edu>
2024-05-20 15:48:06 +0000

    may 20 edits

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,17 @@
 Rails.application.routes.draw do
+  get 'entries/new'
+  get 'entries/create'
+  get 'places/index'
+  get 'places/show'
+  get 'places/new'
+  get 'places/create'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # get("/", { :controller => "articles", :action => "index" })
+  root 'places#index'
+  
+  resources :places do
+    resources :entries, only: [:new, :create]
+  end
 end
